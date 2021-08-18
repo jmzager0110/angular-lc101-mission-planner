2536b23e5f31fe5229d85493a081f717886d62ae
Jessica Zager <jmzager@gmail.com>
2021-08-17 20:09:31 -0500

    Bonus Mission

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./equipment.component.css']
 })
 export class EquipmentComponent implements OnInit {
+  
+  equipment: string[] = ['Habitat Dome', 'Drones', 'Food Containers', 'Oxygen Tanks'];
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  addEq(eqName: string) {
+    this.equipment.push(eqName)
+  }
+
+  removeEq(eqName: string) {
+    let index = this.equipment.indexOf(eqName);
+    this.equipment.splice(index, 1);
+  }
+
+  eqBeingEdited: string = null;
+
+  editEq(eqName: string) {
+    this.eqBeingEdited = this.equipment;
+  }
+
+
 
 }
